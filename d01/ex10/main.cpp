b88b52e67f262381484b6e0cf20806777af3e5fc
fxrc <Freccia@users.noreply.github.com>
2018-05-12 14:52:57 +0200

    d01/ex10 testets

--- d01/ex10/main.cpp
+++ d01/ex10/main.cpp
@@ -6,39 +6,62 @@
 /*   By: lfabbro <>                                 +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/05/12 11:10:47 by lfabbro           #+#    #+#             */
-/*   Updated: 2018/05/12 14:08:54 by lfabbro          ###   ########.fr       */
+/*   Updated: 2018/05/12 14:52:28 by lfabbro          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
-#include "Cat.hpp"
+#include <fstream>
+#include <stdio.h>
+#include <errno.h>
+
+#define LINELEN		16
 
 /*
-void	usage(std::string name)
+void	catFile(FILE *inFile, FILE* outFile)
 {
-	std::cout << "Usage: " << name << " [inFileName, outFileName]" << std::endl;
-	exit(1);
+	char	line[LINELEN];
+	int		lineno = 1;
+
+	while (fgets(line, LINELEN, inFile)) {
+		if (fputs(line, outFile) == EOF) {
+			perror("Write to stdout failed.");
+			return;
+		}
+
+		++lineno;
+	}
 }
 */
 
 int		main(int ac, char **av)
 {
-	Cat				cat("");
-	bool			ret = true;
+	std::string		str;
+	std::ifstream	inFile;
+	
+	if (ac < 2) {
+		do {
+			getline(std::cin, str);
+			std::cout << str;
+		} while (std::cin.eof() == false);
+		return 0;
+	}
 
-	if (ac > 1) {
-		for (int i=1; i < ac; i++) {
-			cat.setFile(av[i]);
-			do {
-				cat.write();
-			} while (cat.read());
-			cat.close();
+	for (int i=0; i < ac; i++) {
+		inFile.open(av[i]);
+		if (inFile.is_open()) {
+			while (inFile.is_open())
+				inFile >> str;
+				std::cout << str;
+			inFile.close();
+		}
+		else
+		{
+			std::string		err = av[0];
+			err.append(": ");
+			err.append(av[i]);
+			perror(err.c_str());
 		}
 	}
-	else
-		while (ret) {
-			cat.read();
-			cat.write();
-		}
 	return 0;
 }
