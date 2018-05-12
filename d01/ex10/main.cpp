908803deb88aa5fba4dab9092436b1c5aa870eb3
fxrc <Freccia@users.noreply.github.com>
2018-05-12 15:15:28 +0200

    d01/ex10 cat OK, C style

--- d01/ex10/main.cpp
+++ d01/ex10/main.cpp
@@ -6,7 +6,7 @@
 /*   By: lfabbro <>                                 +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/05/12 11:10:47 by lfabbro           #+#    #+#             */
-/*   Updated: 2018/05/12 14:52:28 by lfabbro          ###   ########.fr       */
+/*   Updated: 2018/05/12 15:14:33 by lfabbro          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,54 +14,45 @@
 #include <fstream>
 #include <stdio.h>
 #include <errno.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
 
 #define LINELEN		16
 
-/*
 void	catFile(FILE *inFile, FILE* outFile)
 {
 	char	line[LINELEN];
-	int		lineno = 1;
+	int		n = 1;
 
-	while (fgets(line, LINELEN, inFile)) {
-		if (fputs(line, outFile) == EOF) {
-			perror("Write to stdout failed.");
-			return;
-		}
-
-		++lineno;
+	while (n > 0) {
+		n = read(fileno(inFile), line, LINELEN);
+		write(fileno(outFile), line, n);
 	}
 }
-*/
 
 int		main(int ac, char **av)
 {
-	std::string		str;
-	std::ifstream	inFile;
+	FILE	*inFile;
+	int		ret = 0;
 	
-	if (ac < 2) {
-		do {
-			getline(std::cin, str);
-			std::cout << str;
-		} while (std::cin.eof() == false);
+	if (ac == 1) {
+		catFile(stdin, stdout);
 		return 0;
 	}
 
-	for (int i=0; i < ac; i++) {
-		inFile.open(av[i]);
-		if (inFile.is_open()) {
-			while (inFile.is_open())
-				inFile >> str;
-				std::cout << str;
-			inFile.close();
+	for (int i=1; i < ac; i++) {
+		if ((inFile = fopen(av[i], "r")) != NULL) {
+			catFile(inFile, stdout);
 		}
-		else
-		{
+		else {
 			std::string		err = av[0];
 			err.append(": ");
 			err.append(av[i]);
 			perror(err.c_str());
+			ret = 1;
 		}
 	}
-	return 0;
+	return 1;
 }
