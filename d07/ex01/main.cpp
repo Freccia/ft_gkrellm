ff7e8642fd1aa5ce2239d1073b84a882518b6a83
freccia <Freccia@users.noreply.github.com>
2018-06-06 12:23:05 +0200

    refactor d07/ex01

--- d07/ex01/main.cpp
+++ d07/ex01/main.cpp
@@ -3,16 +3,29 @@
 #include <iostream>
 
 template <typename T>
-void	test_func(T x) {
-	std::cout << "f(" << x << ")" << std::endl;
+T	test_templ(T x) {
+	std::cout << "templ(" << x << ")" << std::endl;
+	return x;
+}
+
+std::string		test_str(std::string str) {
+	std::cout << "str(" << str << ")" << std::endl;
+	return str;
+}
+
+int				test_biz(std::string str) {
+	std::cout << "biz(" << str << ")" << std::endl;
+	return str.length();
 }
 
 int		main( void )
 {
-//char arr[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-	char arr[] = "abcdefghijklmnopqrstuvwxyz0123456789";
+	std::string arr[4] = {"Blue", "Red", "Orange", "Yellow"};
+	size_t	size = 4;
 
-	iter(arr, 20, &test_func);
+	::iter(arr, size, &test_templ);
+	::iter(arr, size, &test_str);
+	::iter_bis(arr, size, &test_biz);
 	std::cout << "arr: " << arr << std::endl;
 	return 0;
 }
--- d07/ex01/templates.cpp
+++ d07/ex01/templates.cpp
@@ -6,50 +6,24 @@
 /*   By: lfabbro <>                                 +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/06/05 10:40:27 by lfabbro           #+#    #+#             */
-/*   Updated: 2018/06/05 16:18:57 by lfabbro          ###   ########.fr       */
+/*   Updated: 2018/06/06 12:21:29 by lfabbro          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <string.h>
 
-template <typename T> // this is the template parameter declaration
-T		max(T x, T y)
-{
-	return (x > y) ? x : y;
-}
-
 template <typename T>
-T		min(T x, T y)
+void	iter(T *arr, size_t size, T (*f)(T))
 {
-	return (x < y) ? x : y;
-}
-
-template <typename T>
-void	swap(T &x, T &y)
-{
-	T	k;
-	T	j;
-
-	k = y;
-	j = x;
-	x = k;
-	y = j;
-}
-
-/*
-template <typename T, typename F>
-void	iter(T val, size_t size, F funk)
-{
-	for(size_t i=0; i < size; i++)
-	{
-		funk(val[i]);
+	for(size_t i=0; i < size; i++) {
+		f(arr[i]);
 	}
 }
-*/
 
-template<typename T, typename F>
-void	iter(T *arr, ssize_t size, F (*f)(T &)) {
-	for (int i = 0; i < size; i++) {
+template <typename T, typename F>
+void	iter_bis(T *arr, size_t size, F (*f)(T))
+{
+	for(size_t i=0; i < size; i++) {
 		f(arr[i]);
 	}
 }
