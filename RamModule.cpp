4573a992c777cec7aaf2105d4c16caad57eb28d4
freccia <Freccia@users.noreply.github.com>
2018-06-10 12:24:18 +0200

    wtf Net module hex

--- RamModule.cpp
+++ RamModule.cpp
@@ -6,7 +6,7 @@
 /*   By: lfabbro <>                                 +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/06/09 11:00:47 by lfabbro           #+#    #+#             */
-/*   Updated: 2018/06/09 15:21:32 by lfabbro          ###   ########.fr       */
+/*   Updated: 2018/06/10 11:37:46 by lfabbro          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,8 @@
 #include <mach/task.h>
 #include <QBoxLayout>
 #include <QRect>
+
+#include <tgmath.h>
 
 
 #define BUFF 256
@@ -66,6 +68,8 @@ RamModule::RamModule(int posx, int posy):
 
 	sysctlbyname("hw.memsize", &ramSize, &sizeBrand, NULL, 0);
 
+	this->_phisicalMem = ramSize;
+
 	this->_ramSize =    "RAM:      ";
 	this->_ramSize += std::to_string(ramSize / 1000000000);
 	this->_ramSize +=    "GB";
@@ -76,7 +80,7 @@ RamModule::~RamModule(void) {
 }
 
 void		RamModule::_updateRamUsage(void) {
-	char	memLabel[BUFF];
+	//char	buff[BUFF];
 	mach_msg_type_number_t count = HOST_VM_INFO_COUNT;
 
 	vm_statistics_data_t vmstat;
@@ -92,41 +96,28 @@ void		RamModule::_updateRamUsage(void) {
 	double inactive = vmstat.inactive_count;//  / total;
 	double free = vmstat.free_count ;// / total;
 
-	// TODO CHANGE ME
-	snprintf(memLabel, sizeof(memLabel), "Total: % 3.1f ", total);
-
-	this->_ramUsage = memLabel;
+	//int64_t memUsed = this->_phisicalMem - (vmstat.free_count * 4096);
+	//int64_t virtualMem = this->_phisicalMem - (vmstat.free_count * 4096);
 
 	// TODO CHANGE ME
-	snprintf(memLabel, sizeof(memLabel), "Wired: % 3.1f  Active: % 3.1f  Inactive: % 3.1f  Free: % 3.1f", wired, active, inactive, free);
+	//snprintf(buff, sizeof(buff), "Total: % 3.1f ", total);
+	this->_ramUsage = "Total: ";
+	this->_ramUsage += std::to_string(static_cast<int>(floor(total)));
 
-	this->_ramUsageBis = memLabel;
+	//this->_ramUsage = buff;
 
-	/*
-	int		mib[6];
-	int		pagesize;
-	size_t	length;
+	// TODO CHANGE ME
+	//snprintf(buff, sizeof(buff), "Wired: % 3.1f  Active: % 3.1f  Inactive: % 3.1f  Free: % 3.1f", wired, active, inactive, free);
+	this->_ramUsageBis = "Wired: ";
+	this->_ramUsageBis += std::to_string(static_cast<int>(floor(wired)));
+	this->_ramUsageBis += " Active: ";
+	this->_ramUsageBis += std::to_string(static_cast<int>(floor(active)));
+	this->_ramUsageBis += " Inactive: ";
+	this->_ramUsageBis += std::to_string(static_cast<int>(floor(inactive)));
+	this->_ramUsageBis += " Free: ";
+	this->_ramUsageBis += std::to_string(static_cast<int>(floor(free)));
 
-	mib[0] = CTL_HW;
-	mib[1] = HW_PAGESIZE;
-	length = sizeof (pagesize);
-
-	if (sysctl (mib, 2, &pagesize, &length, NULL, 0) < 0) {
-		return;
-	}
-	*/
-	/*
-	task_basic_info_64_data_t info;
-	unsigned size = sizeof (info);
-	task_info (mach_task_self (), TASK_BASIC_INFO_64, (task_info_t) &info, &size);
-
-	double unit = 1024 * 1024;
-	char memLabel[BUFF];
-	snprintf(memLabel, sizeof(memLabel), "% 3.1f MB\n% 3.1f MB\n% 3.1f MB",
-		vmstat.free_count * pagesize / unit,
-		(vmstat.free_count + vmstat.inactive_count) * pagesize / unit,
-		info.resident_size / unit);
-	*/
+	//this->_ramUsageBis = buff;
 }
 
 void		RamModule::_update(void)
--- tools.cpp
+++ tools.cpp
@@ -6,7 +6,7 @@
 /*   By: lfabbro <>                                 +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/06/09 15:32:24 by lfabbro           #+#    #+#             */
-/*   Updated: 2018/06/09 15:44:53 by lfabbro          ###   ########.fr       */
+/*   Updated: 2018/06/10 12:14:58 by lfabbro          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,25 +14,30 @@
 #include <string>
 #include <sstream>
 #include <iomanip>
+#include <tgmath.h>
 
-template <typename I>
-std::string n2hexstr(I w, size_t hex_len = sizeof(I) << 1)
+std::string c2hex(unsigned char val)
 {
-	static const char* digits = "0123456789ABCDEF";
-	std::string xs( hex_len, '0' );
+	static const char*	digits = "0123456789ABCDEF";
+	char				num[2];
+	size_t				size = sizeof(num);
+	std::string			ret;
 
-	for (size_t i=0, j = (hex_len-1)*4 ; i < hex_len; ++i, j -= 4) {
-		xs[i] = digits[ (w >> j) & 0x0f ];
+	if (val == 0) {
+		ret = "00";
+		return ret;
 	}
-	return xs;
-}
+	if (val < 16) {
+		ret = "0";
+		ret += digits[val];
+		return ret;
+	}
 
-//template< typename T >
-std::string nToHex( unsigned char i )
-{
-	std::stringstream stream;
-	stream << "0x" 
-		<< std::setfill ('0') << std::setw(sizeof(unsigned char)*2) 
-		<< std::hex << i;
-	return stream.str();
+	while (val)
+	{
+		num[--size] = digits[(val % 16)];
+		val /= 16;
+	}
+	ret = num;
+	return (ret);
 }
